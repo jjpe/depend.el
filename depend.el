1f8f0c77ad75a68857d10375b779908083e453e3
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-07-10 14:40:26 +0200

    Bump version to 0.2.0

--- depend.el
+++ depend.el
@@ -4,7 +4,7 @@
 
 (require 'dash) ;; threading macros
 
-(defvar depend/semver "0.1.0"
+(defvar depend/semver "0.2.0"
   "The semantic version of this depend.el release.")
 
 (defvar depend/bin-semver "0.1.0"
