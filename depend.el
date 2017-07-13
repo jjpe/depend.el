65677bb0e8a634de1673dd9748039c9e382a9e76
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-07-13 22:17:27 +0200

    Bump version to 0.6.0

--- depend.el
+++ depend.el
@@ -12,7 +12,7 @@
 (require 'dash) ;; threading macros
 (require 'json)
 
-(defvar depend/semver "0.5.6"
+(defvar depend/semver "0.6.0"
   "The semantic version of this depend.el release.")
 
 (defvar depend/bin-semver "0.1.0"
